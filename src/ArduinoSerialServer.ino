da712626f7f8aa484ddcaa02c6254692a23e37f4
Hans Johnson <hejohnson@wpi.edu>
2016-05-14 12:25:22 -0500

    Updated pins to match physical model

--- src/ArduinoSerialServer.ino
+++ src/ArduinoSerialServer.ino
@@ -2,49 +2,49 @@ bool sent = false;
 
 void setup() {
 	Serial.begin(9600);
-	pinMode(8, INPUT_PULLUP);
-	pinMode(9, INPUT_PULLUP);
-	pinMode(10, INPUT_PULLUP);
-	pinMode(11, INPUT_PULLUP);
-	pinMode(12, INPUT_PULLUP);
 	pinMode(2, INPUT_PULLUP);
 	pinMode(3, INPUT_PULLUP);
+	pinMode(4, INPUT_PULLUP);
+	pinMode(5, INPUT_PULLUP);
+	pinMode(6, INPUT_PULLUP);
+	pinMode(7, INPUT_PULLUP);
+	pinMode(8, INPUT_PULLUP);
 
 }
 
 
 void loop() {
-	if (!digitalRead(8)) {
+	if (!digitalRead(2)) {
 		if (!sent) {
 			Serial.println("On");
 		}
 		sent = true;
-	} else if (!digitalRead(9)) {
+	} else if (!digitalRead(3)) {
 		if (!sent) {
 			Serial.println("Off");
 		}
 		sent = true;
-	} else if (!digitalRead(10)) {
+	} else if (!digitalRead(4)) {
 		if (!sent) {
 			Serial.println("Shutdown");
 		}
 		sent = true;
-	} else if (!digitalRead(11)) {
+	} else if (!digitalRead(5)) {
 		if (!sent) {
 			Serial.println("Kinect");
 		}
 		sent = true;
-	} else if (!digitalRead(12)) {
+	} else if (!digitalRead(6)) {
 		if (!sent) {
 			Serial.println("Projector");
 		}
 		sent = true;
-	} else if (!digitalRead(2)) {
+	} else if (!digitalRead(7)) {
 		if (!sent) {
 			Serial.println("Capture");
 		}
 		sent = true;
-	} else if (!digitalRead(3)) {
+	} else if (!digitalRead(8)) {
 		if (!sent) {
 			Serial.println("ResetBG");
 		}
